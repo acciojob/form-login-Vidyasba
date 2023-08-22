8db9532c8a300ce2e1a71fac6de61b127ae122b4
Vidyasba <116746304+Vidyasba@users.noreply.github.com>
2023-08-22 11:30:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+
+let formref= document.getElementById("form1")
 function getFormvalue() {
     //Write your code here
-	const fname=document.getElementByTag("input")[0];
-	const lname=document.getElementBytag("input")[1];
-window.alert(fname +" "+ lname);
+	// const fname=document.getElementByTag("input")[0];
+	const fname=formref["fname"].value;
+	// const lname=document.getElementBytag("input")[1];
+	const lname=formref["lname"].value;
+	alert(fname +" "+ lname);
 }
